62516b52a0fe0f35a8a87ba01e0d874a010c798c
Unkyung Lee <uklee1029@gmail.com>
2020-06-29 21:36:48 -0500

    global variables

---
+++ R/globals.R
@@ -0,0 +1,3 @@
+utils::globalVariables(c("results", "logit.est.second", "logit.true.second",
+                         "x", "simu.dat", "logDiff", "TMS", "person", "para.TMS",
+                         "se", "outdat", "combi.res"))
