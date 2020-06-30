6c89476a13ed12bb8eea8fd004dbaa4438f40c7b
Unkyung Lee <uklee1029@gmail.com>
2020-06-29 22:26:46 -0500

    test code updates

--- R/globals.R
+++
@@ -1,3 +0,0 @@
-utils::globalVariables(c("results", "logit.est.second", "logit.true.second",
-                         "x", "simu.dat", "logDiff", "TMS", "person", "para.TMS",
-                         "se", "outdat", "combi.res"))
